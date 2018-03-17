3179ef090a74788666bd87680b1f943d583bda82
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-03-17 16:09:01 +0530

    Added code to component with inline html template

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',
+  template: `
+  Employee List : 
+  <div *ngFor = "let employee of employees" >
+       {{employee.id}} 
+  </div>
+  `,
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+
+  private employees: Employee[] =
+    [
+      { id: "sagar", age: 33, image: "/images/sagar.jpg" },
+      { id: "sunil", age: 34, image: "/images/sunil.jpg" }
+    ];
 
   constructor() { }
 
