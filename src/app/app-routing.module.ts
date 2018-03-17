6ebcd5d2b24f307f4b3043064c8272ce8f938458
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-03-17 20:42:05 +0530

    Creating  routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EmployeeListComponent} from './employee-list/employee-list.component'
+import {EmployeeCreateComponent} from './employee-create/employee-create.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"list" , component : EmployeeListComponent },
+  {path:"create" , component : EmployeeCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
