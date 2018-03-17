e7138936ccc001949e3325219908588d063c9e19
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-03-17 20:27:49 +0530

    Adding routing (to an existing app)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {EmployeeServiceService} from  './employee-service.service';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
@@ -15,7 +15,8 @@ import { EmployeeCreateComponent } from './employee-create/employee-create.compo
   ],
   imports: [
     BrowserModule, 
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [EmployeeServiceService],
   bootstrap: [AppComponent]
