06e1fcee36a4464fe2e54a08198fe4e09cf337b8
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-03-17 09:25:48 +0530

    Adding a Service

---
+++ src/app/employee-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee';
+
+@Injectable()
+export class EmployeeServiceService {
+
+  constructor() { }
+
+  private employees : Employee[] =
+    [
+      { id: "sagar", age: 33, image: "/images/sagar.jpg" },
+      { id: "sunil", age: 34, image: "/images/sunil.jpg" }
+    ];
+
+  getAll() {
+    return this.employees;
+  }
+
+}
