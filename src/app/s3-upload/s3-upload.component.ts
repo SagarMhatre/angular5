16f07d425f83d9a339eb439d796e0416ce3efc56
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-04-11 08:27:49 +0530

    s3 File Security

--- src/app/s3-upload/s3-upload.component.ts
+++ src/app/s3-upload/s3-upload.component.ts
@@ -16,7 +16,11 @@ export class S3UploadComponent implements OnInit {
   }
 
   upload(){
-    Storage.put('test.txt', 'Hello')
+    Storage.put('test6.txt', 'Hello World')
+    .then (result => console.log(result))
+    .catch(err => console.log(err));
+
+    Storage.vault.put('vault-test7.txt', 'Vault Hello World')
     .then (result => console.log(result))
     .catch(err => console.log(err));
   }
